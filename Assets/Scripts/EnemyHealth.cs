b4ce82a25f0294cb4f6205a1849216749ee3130d
khoo234 <kholilblogger234@gmail.com>
2026-01-31 13:43:07 +0700

    sss

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float maxHealth = 50f;
+    public float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        Debug.Log(gameObject.name + " HP: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("? Enemy Mati");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ProjectileDamage.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class ProjectileDamage : MonoBehaviour
+{
+    public float damage = 10f;
+    public float lifeTime = 3f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("EnemyRed") &&
+            !other.CompareTag("EnemyBlue") &&
+            !other.CompareTag("EnemyGreen"))
+            return;
+
+        if (IsEffective(other.tag))
+        {
+            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+            }
+        }
+        else
+        {
+            Debug.Log("? Salah elemen, tidak ada damage");
+        }
+
+        Destroy(gameObject);
+    }
+
+    bool IsEffective(string enemyTag)
+    {
+        // Hijau -> Merah
+        if (CompareTag("ProjectileGreen") && enemyTag == "EnemyRed")
+            return true;
+
+        // Merah -> Biru
+        if (CompareTag("ProjectileRed") && enemyTag == "EnemyBlue")
+            return true;
+
+        // Biru -> Hijau
+        if (CompareTag("ProjectileBlue") && enemyTag == "EnemyGreen")
+            return true;
+
+        return false;
+    }
+}
